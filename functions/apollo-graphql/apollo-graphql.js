b725142247f8eac94c865ec3cdd607787c492850
Rem Kim <kim.rmi.93@gmail.com>
2019-11-21 16:19:24 -0500

    serverless functions dev settings and netlify lambda for build

--- functions/apollo-graphql/apollo-graphql.js
+++ functions/apollo-graphql/apollo-graphql.js
@@ -1,8 +1,9 @@
-const { ApolloServer, gql } = require('apollo-server-lambda')
+const { ApolloServer, gql } = require("apollo-server-lambda");
 
 const typeDefs = gql`
   type Query {
     hello: String
+    bye: String
     allAuthors: [Author!]
     author(id: Int!): Author
     authorByName(name: String!): Author
@@ -12,35 +13,41 @@ const typeDefs = gql`
     name: String!
     married: Boolean!
   }
-`
+`;
 
 const authors = [
-  { id: 1, name: 'Terry Pratchett', married: false },
-  { id: 2, name: 'Stephen King', married: true },
-  { id: 3, name: 'JK Rowling', married: false }
-]
+  { id: 1, name: "Terry Pratchett", married: false },
+  { id: 2, name: "Stephen King", married: true },
+  { id: 3, name: "JK Rowling", married: false },
+  { id: 4, name: "Rem Kim", married: true }
+];
 
 const resolvers = {
   Query: {
     hello: (root, args, context) => {
-      return 'Hello, world!'
+      return "Hello, world!";
+    },
+    bye: (root, args, context) => {
+      return "Bye, world2!";
     },
     allAuthors: (root, args, context) => {
-      return authors
+      return authors;
     },
     author: (root, args, context) => {
-      return
+      return;
     },
     authorByName: (root, args, context) => {
-      console.log('hihhihi', args.name)
-      return authors.find(x => x.name === args.name) || 'NOTFOUND'
+      console.log("hihhihi", args.name);
+      return authors.find(x => x.name === args.name) || "NOTFOUND";
     }
   }
-}
+};
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
-})
+  resolvers,
+  playground: true,
+  introspection: true
+});
 
-exports.handler = server.createHandler()
+exports.handler = server.createHandler();
